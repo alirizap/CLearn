54d8d171ea011c13c511ee693c1abf2ba0e0fe65
Alirezap <pishdaralirza@gmail.com>
2021-06-12 22:50:14 +0430

    add program 'Memory allocator sample'

---
+++ ch1/memory1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    // This pointer will hold the 
+    // base address of the block created
+    int n, i;
+
+    n = 5;
+    printf("Enter number of elements: %d\n", n);
+
+    int *ptr = malloc(n * sizeof(int));
+
+    if(ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    } else {
+        printf("Memeory successfully allocated using molloc.\n");
+
+        for (i = 0; i < n; i++) {
+            ptr[i] = i + 1;
+        } 
+
+        printf("The elements of the array are: \n");
+
+        free(ptr);
+        for(i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+
+        return 0;
+    }
+}
