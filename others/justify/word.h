d2796269f2adce20e43cf9660d2122b68ef390d6
alirizap <pishdaralirza@gmail.com>
2021-06-16 22:05:01 +0430

    Add justify program

---
+++ others/justify/word.h
@@ -0,0 +1,11 @@
+
+#ifndef WORD_H
+#define WORD_H
+
+// read_word: Reads the next word from the input and strores
+// in the word. makes word empty if word could be read becuase 
+// of end-of-file Truncates the word if its length exceeds len
+
+void read_word(char *word, int len);
+
+#endif
